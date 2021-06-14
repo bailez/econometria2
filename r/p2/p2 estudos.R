710a89399186ca71a03fdb4418cacb0329d273b7
Felipe Bailez <bailez@usp.br>
2021-06-13 21:46:46 -0300

    upload rproject

---
+++ r/p2/p2 estudos.R
@@ -0,0 +1,8 @@
+# montando a matriz y
+y <- matrix(c(800, 1160, 1580, 2010, 1890, 2600, 2070, 1890, 1830, 1740, 
+              1380, 1060), ncol = 1, nrow = 12, byrow = F)
+
+# montando a matriz x
+x2 <- matrix(c(2,4,6,8,7,12,11,10,9,8,6,4), ncol=1, nrow=12, byrow=F)
+x3 <- matrix(c(.8,.7,.5,.4,.2,.2,.8,.7,.6,.1,.5,.4), ncol=1, nrow=12, byrow=F)
+x <- matrix(c(1,1,1,1,1,1,1,1,1,1,1,1,X2,X3), ncol=3, nrow=12, byrow=F)
